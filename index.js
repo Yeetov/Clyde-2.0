d23597fe73be7256b3bb7eff0af909c250c1956b
FatalFang <69159547+Yeetov@users.noreply.github.com>
2021-07-22 14:14:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,148 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+  console.log('I am ready! Logged into Client!')
+});
+
+//`https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`
+//Kick command
+
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('!kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+  }
+});
+
+//Ban command
+
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // if the message content starts with "!ban"
+  if (message.content.startsWith('!ban')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+         */
+        member
+          .ban({
+            reason: 'They were bad!',
+          })
+          .then(() => {
+            // We let the message author know we were able to ban the person
+            message.reply(`Successfully banned ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to ban the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to ban the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+    } else {
+      // Otherwise, if no user was mentioned
+      message.reply("You didn't mention the user to ban!");
+    }
+  }
+});
+//Discord Status
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("ClydeTV | Running discord.js", { type: "WATCHING" })
+});
+//Join Server message
+client.on('guildCreate', guild => {
+guild.systemChannel.send('Hello, thank you for adding me! If you need help, just use `!help` :)')
+})
+  
+//Help Command
+client.on('message', message => {
+  // If message is ping
+  if (message.content === '!help') {
+    // Send pong back
+    message.reply('Thank you for asking for help! The commands are: `!ban <user>` and `!kick <user>`! More commands are coming, this bot is still in development. <a:version:867324402151718932><:clyde:867483292538372106>');
+  }
+});
+//Login to bot
+client.login(process.env.TOKEN);
+//Keep Alive
+const express = require('express');
+const server = express();
+ 
+server.all('/', (req, res) => {
+  res.send(`OK`)
+})
+ 
+function keepAlive() {
+  server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
+}
+
+//Invite Command
+
+client.on('message', message => {
+  // If message is ping
+  if (message.content === '!invite') {
+    // Send pong back
+    message.reply('https://dsc.gg/clyde2.0 <:clyde:867483292538372106>');
+  }
+});
