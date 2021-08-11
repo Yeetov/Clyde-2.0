c7d860729772528b0f2adf57fd190ddf37de8f5d
Fatal Fang <69159547+Yeetov@users.noreply.github.com>
2021-08-11 16:16:23 +0000

    Added server count command

--- index.js
+++ index.js
@@ -7,108 +7,12 @@ client.on('ready', () => {
 });
 
 //`https://nekobot.xyz/api/imagegen?type=clyde&text=${text}`
-//Kick command
 
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Successfully kicked ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to kick the member');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("That user isn't in this guild!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("You didn't mention the user to kick!");
-    }
-  }
-});
-
-//Ban command
-
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // if the message content starts with "!ban"
-  if (message.content.startsWith('!ban')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Ban the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         * Read more about what ban options there are over at
-         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
-         */
-        member
-          .ban({
-            reason: 'They were bad!',
-          })
-          .then(() => {
-            // We let the message author know we were able to ban the person
-            message.reply(`Successfully banned ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to ban the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to ban the member');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("That user isn't in this guild!");
-      }
-    } else {
-      // Otherwise, if no user was mentioned
-      message.reply("You didn't mention the user to ban!");
-    }
-  }
-});
 //Discord Status
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("ClydeTV | Running discord.js", { type: "WATCHING" })
+  client.user.setActivity(`${client.guilds.cache.size} Servers | Running discord.js`, { type: "WATCHING" })
 });
 //Join Server message
 client.on('guildCreate', guild => {
@@ -120,7 +24,7 @@ client.on('message', message => {
   // If message is ping
   if (message.content === '!help') {
     // Send pong back
-    message.reply('Thank you for asking for help! \nThe commands are:\n`!ban <user>` Bans a user from the server\n`!kick <user>` Kicks a user from the server\n`!invite` Sends discord bot invite to add the bot\nMore commands are coming, this bot is still in development. <a:version:867324402151718932><:clyde:867483292538372106> \nTutorial: https://youtu.be/ew8YMSCt4NE\n\nUse `!help <page number>` for more commands.');
+    message.reply('Thank you for asking for help! \nThe commands are:\n`!invite` Sends discord bot invite to add the bot\n`!support` Sends support server invite to ask for support and hang out with the community\n\nMore commands are coming, this bot is still in development. <a:version:867324402151718932><:clyde:867483292538372106> \nTutorial: https://youtu.be/ew8YMSCt4NE\n\n`!count` Gives the bots server count.\n\nUse `!help <page number>` for more commands.');
   }
 });
 
@@ -186,21 +90,20 @@ client.on('message', message => {
  }
 });
 
+//Support Command
 
-//Message Log
-client.on('messageUpdate', async(oldMessage,newMessage)=>{
- require('./messageUpdate')(oldMessage,newMessage)
-})
-client.on('messageDelete', async(message)=>{
- const {MessageEmbed} = require('discord.js')
-module.exports=async(message)=>{
-  let embed = new MessageEmbed()
-  .setTitle(`New message deleted!`)
-  .setDescription(`**The user, ${message.author.tag} has deleted a message in <#${message.channel.id}>**`)
-  .addField(`Content`, message.content,true)
-  let channel = message.guild.channels.cache.find(ch=ch.name==='delete-message-logs')
-  if(!channel)return;
-  channel.send(embed)
+client.on('message', message => {
+  if (message.content === '!support'){
+    message.reply('Join this server for support, if you do not trust the link that is not discord, look at this FAQ: <https://dsc.gg/legal/privacy>\n\nSupport Server: https://dsc.gg/ffd')
+  }
+});
 
-}
-})
+//Member Count Command
+
+client.on('message', message => {
+  // If message is ping
+  if (message.content === '!count') {
+    // Send pong back
+    message.reply(`I am in ${client.guilds.cache.size} servers!`);
+  }
+});
